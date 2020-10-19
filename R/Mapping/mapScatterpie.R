7bb28b2a96189c068bbbba6e7e417f97a8947523
akoontz11 <austinkoontz11@gmail.com>
2020-10-19 14:22:21 -0600

    Updates to including elevations on the map

--- R/Mapping/mapScatterpie.R
+++ R/Mapping/mapScatterpie.R
@@ -4,8 +4,14 @@ library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(maps)
+# raster, for getting elevation data
+library(raster)
+# scatterpie, for plotting pie charts
 library(scatterpie)
+# ggnewscale, a way to trick ggplot2 into using multiple color scales in a single plot
+library(ggnewscale)
 
+# Cluster memberships----
 # Read in a csv containing population names, latitudes, and longitudes
 setwd("~/kaiser/Code/R/Mapping/")
 pops <- read.csv("Coordinates.csv", header=T, sep=",")
@@ -28,7 +34,7 @@ NEV <- tapply(k7$P7, k7$Group, mean)
 
 # Bind the vectors together to get a reduced Q matrix, with one row for each population
 mean.Ks <- cbind(SRP,JAR,OWY,MAG,DOM,NEV,PAR)
-# For some reason, the above code will locate domensis populations before maguirei, which is out oy sync with the pops location matrix
+# For some reason, the above code will locate domensis populations before maguirei, which is out of sync with the pops location matrix
 # The line below reorders rows in this reduced Q matrix to match the order of rows in pops
 mean.Ks <- mean.Ks[c((1:6),(10:13),(7:9),(14:17)),]
 # For ease of presentation, populations are rounded to their nearest quarter (i.e., only display 0.25, 0.5, 0.75. 1.0)
@@ -48,7 +54,7 @@ rownames(mean.Ks) <- NULL
 
 # Bind the longitudes and latitudes with the Q matrix values. Add a column of radius values for the pie charts to be plotted (all 0.25)
 pops <- cbind(pops, mean.Ks)
-radius <- rep(0.2, 17)
+radius <- rep(0.3, 17)
 pops <- cbind(pops, radius)
 
 # Plotting----
@@ -59,18 +65,42 @@ greatBasin <- st_as_sf(map("state", regions = c("nevada", "utah", "idaho", "oreg
 greatBasin <- cbind(greatBasin, st_coordinates(st_centroid(greatBasin)))
 greatBasin
 
+# Elevations
+# Load elevations using Rdata file
+load("map.Rdata")
+# Obtaining geographic data from GADM data set
+# US <- getData("GADM", country="USA", level=1)
+# # Subset by state name
+# greatBasinraster <- US[US$NAME_1 %in% c("Oregon", "Idaho", "Nevada", "Utah"),]
+# # Get elevations
+# elev <- getData("alt", country = "USA", mask = TRUE)
+# # Crop elevation dataset by greatBasinraster
+# elev <- crop(elev[[1]], greatBasinraster)
+# elev <- mask(elev, greatBasinraster)
+# # Convert raster to data.frame, for ggplot
+# elev_df <- raster::as.data.frame(elev, xy=TRUE)
+# elev_df <- elev_df[!is.na(elev_df$USA1_msk_alt),]
+
 # First line plots baseline Great Basin map
 ggplot(data = greatBasin) +
+  theme_void() +
   geom_sf() +
+  # Using geom_tile to include elevation data
+  geom_tile(data = elev_df, aes(x = x, y = y, fill = USA1_msk_alt), alpha = 0.45, show.legend = FALSE) +
+  # Use colors to paint elevation gradient
+  scale_fill_gradient(low = "black", high = "white", name = "Elevation") +
+  geom_sf(fill = NA) +
   # State labels
   geom_text(data = greatBasin, aes(X, Y, label = c("NV", "UT", "ID", "OR")), size = 3) +
   # Specify plotting window
-  coord_sf(xlim = c(-126, -108), ylim = c(34.5, 49.5), expand = FALSE) + 
+  coord_sf(xlim = c(-126, -108), ylim = c(34.5, 49.5), expand = FALSE) +
+  # Generate a new scale fill (for scatterpie; ggplot2 typically allows only one scale per plot)
+  new_scale_fill() +
   # Scatterpie line, using the built pops matrix as data
-  geom_scatterpie(data = pops, 
+  geom_scatterpie(data = pops,
                   aes(x=longitude, y=latitude, r = radius),
                   legend_name = "Clusters",
-                  cols = c("SRP","JAR","OWY","MAG","DOM","NEV","PAR"), 
+                  cols = c("SRP","JAR","OWY","MAG","DOM","NEV","PAR"),
                   alpha = 0.5) +
   scale_fill_manual(
     breaks = c("SRP","JAR","OWY","MAG","DOM","NEV","PAR"),
@@ -82,8 +112,34 @@ ggplot(data = greatBasin) +
                "DOM" = "#7570B3",
                "NEV" = "#666666",
                "PAR" = "#2171B5"
-    ) 
-)
+    )
+  )
+
+# # First line plots baseline Great Basin map
+# ggplot(data = greatBasin) +
+#   geom_sf() +
+#   # State labels
+#   geom_text(data = greatBasin, aes(X, Y, label = c("NV", "UT", "ID", "OR")), size = 3) +
+#   # Specify plotting window
+#   coord_sf(xlim = c(-126, -108), ylim = c(34.5, 49.5), expand = FALSE) + 
+#   # Scatterpie line, using the built pops matrix as data
+#   geom_scatterpie(data = pops, 
+#                   aes(x=longitude, y=latitude, r = radius),
+#                   legend_name = "Clusters",
+#                   cols = c("SRP","JAR","OWY","MAG","DOM","NEV","PAR"), 
+#                   alpha = 0.5) +
+#   scale_fill_manual(
+#     breaks = c("SRP","JAR","OWY","MAG","DOM","NEV","PAR"),
+#     labels = c("cusickiana_Idaho","cusickiana_Jarbidge","cusickiana_owyhee","maguirei","domensis","nevadensis_Troy", "parryi"),
+#     values = c("SRP" = "#8C510A",
+#                "JAR" = "#D95F02",
+#                "OWY" = "#E7298A",
+#                "MAG" = "#66A61E",
+#                "DOM" = "#7570B3",
+#                "NEV" = "#666666",
+#                "PAR" = "#2171B5"
+#     ) 
+# )
 
 # ggmap----
 library(ggmap)
