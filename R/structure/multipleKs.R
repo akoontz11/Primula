658331022d2c8ab793b9ef1353b3f25ffb0c68b5
akoontz11 <austinkoontz11@gmail.com>
2021-10-04 21:04:42 -0500

    Updates to plot 3 K values within main STRUCTURE plot

--- R/structure/multipleKs.R
+++ R/structure/multipleKs.R
@@ -1,4 +1,4 @@
-setwd("~/kaiser/Primula/code/structure/JuneSubset/CLUMPAK/")
+# Code for plotting multiple STRUCTURE outputs vertically
 
 # load in ks from clummpack files
 k2 <- read.csv("K2.csv", sep = '', header = F)
@@ -56,18 +56,26 @@ klist_6 <- list(x[,1:2], x[,3:5], x[,6:9], x[,10:14], x[15:20])
 klist_12 <- list(x[,21:27], x[,28:35], x[,36:44], x[,45:54], x[55:65])
 klist_16 <- list(x[,66:77], x[,78:90], x[,91:104], x[,105:119], x[,120:135])
 
+# Subset of individuals for "optimal" K values
+klist_optimal <- list(x[,10:14], x[,21:27], x[,91:104])
+
 # Names given to groups
-groups <- c("ck_Idaho","ck_Jarbidge","ck_Owyhee","maguirei","domensis","nv_GRBA","nv_Troy", "parryi")
+groups <- c("cusickiana_SRP","cusickiana_Jarbidge","cusickiana_Owyhee","maguirei","domensis","nevadensis_GRBA","nevadensis_Troy", "parryi")
 
 # Matrix for line positions
 x.start <- c(0, 25.3, 30.5, 34.8, 53.7, 69.6, 74.7, 78.9)
 x.end <- c(25, 30.2, 34.5, 53.4, 69.3, 74.4, 78.6, 86.2)
 line.layout <- cbind(x.start, x.end)
 
-dev.off()
 structure_plot(ninds = 82, klist_6, groups, line.layout, Ks=2:6)
 structure_plot(ninds = 82, klist_12, groups, line.layout, Ks=7:11)
 structure_plot(ninds = 82, klist_16, groups, line.layout, Ks=12:16)
+
+# Create a list of titles for K=5/7/14, to be used above each particular K value
+titles <- c("K = 5 (Evanno method)", "K = 7 (Recommended K)", "K = 14 (Pritchard method, most likely K)")
+
+# Plot three different "optimal" K values
+structure_plot_titles(ninds = 82, klist_optimal, groups, line.layout, Ks=c(5,7,14), titles = titles)
 
 #######################################################
 # functions needed
@@ -97,6 +105,37 @@ plot_q_per_chain <- function(kqlist, L, Ks, ...){
     title(main = paste("K =", Ks[i], sep = ' '), line=0.2, cex.main=1.6)
     # y axis
     axis(2, at = c(0, 0.25, 0.5, 0.75, 1), cex.axis = 1, las = 2, pos = -0.02)
+    # lines
+    for(j in 1:nrow(L)){
+      lines(x = c(L[j,1], L[j,2]), y = c(-0.06,-0.06), lwd = 1.8, col = "black", xpd = NA)
+    }
+  }
+}
+
+# Functionally identical to above command, but able to accept unique titles for each plot
+structure_plot_titles <- function(ninds = 82, klist, names, L, Ks, titles){
+  # define colors (16, for maximum number of Ks)
+  cols <- c("#2171B5","#D95F02","#7570B3","#E7298A","#66A61E","#8C510A","#666666","#B3E2CD","#FDCDAC","#CBD5E8","#F4CAE4","#E6F5C9","#FFF2AE","#F1E2CC","#CCCCCC")
+  # locations of each column
+  b <- as.data.frame(matrix(ncol = 1, nrow = ninds))
+  b[,1] <- barplot(t(klist[[1]][1]), beside= F, col= cols, cex.name= 1, cex.axis= 1.2, border = 1, space = 0.05, xaxt = 'n', yaxt = 'n', cex.lab = 1, cex.main=1.6)
+  # plot
+  plot_q_per_chain_titles(klist, L, Ks, titles)
+  # Add labels beneath lowermost plot
+  text(cex = 1.35, x = c(13, 25, 29.5, 44, 62, 69.5, 74.5, 82), y = -0.22, labels = names, xpd=NA, srt=30, font=3)
+}
+
+# Functionally identical to above command, but able to accept unique titles for each plot
+plot_q_per_chain_titles <- function(kqlist, L, Ks, titles, ...){
+  # Define colors
+  cols <- c("#2171B5","#D95F02","#7570B3","#E7298A","#66A61E","#8C510A","#666666","#B3E2CD","#FDCDAC","#CBD5E8","#F4CAE4","#E6F5C9","#FFF2AE","#F1E2CC","#CCCCCC")
+  par(mfrow = c(length(kqlist),1), mar = c(2,1,1.3,1) + 0.1, oma = c(5,0,1,0), mgp = c(2,1,0))
+  for(i in 1:length(kqlist)){
+    # main plot call, and title call
+    barplot(t(kqlist[[i]]), beside= F, col= cols, border = 1, space = 0.05, xaxt = 'n', yaxt = 'n', cex.lab = 1.2)
+    title(main = titles[i], line=0.2, cex.main=1.6)
+    # y axis
+    axis(2, at = c(0, 0.25, 0.5, 0.75, 1), cex.axis = 1.3, las = 2, pos = -0.02)
     # lines
     for(j in 1:nrow(L)){
       lines(x = c(L[j,1], L[j,2]), y = c(-0.06,-0.06), lwd = 1.8, col = "black", xpd = NA)
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Overview
 
 This repository contains the scripts and parameter files used for the genomic analysis of the <em>Primula cusickiana</em> species complex. 
-It is continuously being updated as the manuscript is in preparation. 
 
 ## ipyrad 
 The `ipyrad` directory contains 3 folders, each corresponding to a unique run of the [ipyrad](https://ipyrad.readthedocs.io/en/latest/) software pipeline. Directories contain the relevant `params` files used for each run, as well as relevant outfiles (although not all output files are included, as this is unecessary and would require too much space).
@@ -10,17 +9,19 @@ The `PreliminaryRuns` directory contains the ipyrad barcode and parameters files
 
 In addition, `ComplexWide` and `MaguireiOnly` include folders which contain the files used for F<sub>ST</sub> calculations (using [VCFtools](https://vcftools.github.io/index.html)).
 
+The ComplexWide folder also contains the JuneSubset.nex file, which was passed into the software SplitsTree to generate the split network diagram shown in the manuscript.
+
 ## R
 The `R` directory contains the R script used in data analysis. This includes the plotting code for STRUCTURE plots (in `structure`) and DAPC (in `DAPC`). Code used to generate Jaccard distribution of samples is included in the `JaccardDistances.R` script. 
 
-Additionally, included are R scripts used in intial data exploration.
+Additionally included are R scripts used in intial data exploration.
 
 ## Mapping
 
 The `Mapping` directory includes the Python script and .csv file used to generate the map of samples with the cluster membership at STRUCTURE output K=7. Credit goes to Dr. Carol Rowe for compiling this Python script!
 
 ## STRUCTURE
-The `structure` directory contains folders holding the `mainparams` and `extraparams` files for two STRUCTURE runs: the complex wide analysis, and the analysis looking at strictly maguirei samples. Additionally, output files (.str, .stru, and .vcf) are included, as well as the BASH script used to run STRUCTURE.
+The `structure` directory contains folders holding the `mainparams` and `extraparams` files for two STRUCTURE runs: the complex wide analysis, and the analysis looking at strictly maguirei samples. Additionally, output files (.str, .stru, .vcf, and .nex) are included, as well as the BASH script used to run STRUCTURE.
 
 # Notes
 For any questions regarding these scripts, please contact Austin Koontz (either through GitHub or via email). Other scripts/parameter files used in the analysis of this dataset can be made available upon request.
